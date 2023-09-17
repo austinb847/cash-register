80c8720a69946b635df2efa28adc79139cbc9d56
austinb847 <austinb847@icloud.com>
2023-09-17 13:13:12 -0500

    fix: fix issue with jest and typescript configuration

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,8 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
+  preset: "ts-jest",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
