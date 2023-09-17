00be6e71b4cf34080053f6c94f676830a01d20b5
austinb847 <austinb847@icloud.com>
2023-09-17 13:13:51 -0500

    fix: component test

--- app/__tests__/components/CashRegister.test.tsx
+++ app/__tests__/components/CashRegister.test.tsx
@@ -1,5 +1,4 @@
-import { render, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render, fireEvent, screen } from "@testing-library/react";
 import CashRegister from "../../components/CashRegister";
 
 // Mock the window.alert function to test when it is called
@@ -7,29 +6,29 @@ jest.spyOn(window, "alert").mockImplementation(() => {});
 
 describe("[Component]: CashRegister", () => {
   it("updates the total amount when adding money", () => {
-    const { getByTestId } = render(<CashRegister />);
+    render(<CashRegister />);
 
-    fireEvent.click(getByTestId("add-money-20"));
-    fireEvent.click(getByTestId("add-money-20"));
+    fireEvent.click(screen.getByTestId("add-money-20"));
+    fireEvent.click(screen.getByTestId("add-money-20"));
 
     // Check if the total amount is updated correctly
-    expect(getByTestId("total-amount-text")).toHaveTextContent("$40");
+    expect(screen.getByTestId("total-amount-text")).toHaveTextContent("$40");
   });
 
   it("dispenses change correctly", () => {
-    const { getByRole, getByLabelText, getByTestId } = render(<CashRegister />);
+    render(<CashRegister />);
 
     // Add some money
-    fireEvent.click(getByTestId("add-money-20"));
-    fireEvent.click(getByTestId("add-money-10"));
-    fireEvent.click(getByTestId("add-money-5"));
+    fireEvent.click(screen.getByTestId("add-money-20"));
+    fireEvent.click(screen.getByTestId("add-money-10"));
+    fireEvent.click(screen.getByTestId("add-money-5"));
 
     // Enter a change amount
-    const inputElement = getByLabelText("Change Amount:");
+    const inputElement = screen.getByLabelText("Change Amount:");
     fireEvent.change(inputElement, { target: { value: "25" } });
 
     // Check if the "Change" button is enabled
-    const changeButton = getByRole("button", { name: /Change/i });
+    const changeButton = screen.getByRole("button", { name: /Change/i });
     expect(changeButton).toBeEnabled();
 
     // Dispense change
@@ -44,19 +43,19 @@ describe("[Component]: CashRegister", () => {
   });
 
   it("shows an error when change cannot be made", () => {
-    const { getByTestId, getByRole, getByLabelText } = render(<CashRegister />);
+    render(<CashRegister />);
 
     // Add some money
-    fireEvent.click(getByTestId("add-money-20"));
-    fireEvent.click(getByTestId("add-money-10"));
-    fireEvent.click(getByTestId("add-money-5"));
+    fireEvent.click(screen.getByTestId("add-money-20"));
+    fireEvent.click(screen.getByTestId("add-money-10"));
+    fireEvent.click(screen.getByTestId("add-money-5"));
 
     // Enter a change amount that cannot be made
-    const inputElement = getByLabelText("Change Amount:");
+    const inputElement = screen.getByLabelText("Change Amount:");
     fireEvent.change(inputElement, { target: { value: "50" } });
 
     // Check if the "Change" button is enabled
-    const changeButton = getByRole("button", { name: /Change/i });
+    const changeButton = screen.getByRole("button", { name: /Change/i });
     expect(changeButton).toBeEnabled();
 
     // Dispense change (which should raise an error)
@@ -69,12 +68,12 @@ describe("[Component]: CashRegister", () => {
     const { getByTestId } = render(<CashRegister />);
 
     // Add some money
-    fireEvent.click(getByTestId("add-money-20"));
+    fireEvent.click(screen.getByTestId("add-money-20"));
 
     // Remove some money
-    fireEvent.click(getByTestId("remove-money-20"));
+    fireEvent.click(screen.getByTestId("remove-money-20"));
 
     // Check if the total amount is updated correctly
-    expect(getByTestId("total-amount-text")).toHaveTextContent("$0");
+    expect(screen.getByTestId("total-amount-text")).toHaveTextContent("$0");
   });
 });
